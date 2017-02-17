f27b0b0091a08902a0cfb7806afccf18f3f59d22
917K <ivan.magdjuk@gmail.com>
2017-02-17 23:06:31 +0200

    initial commit

---
+++ frontend/models/SignupForm.php
@@ -0,0 +1,68 @@
+<?php
+namespace frontend\models;
+
+use Yii;
+use yii\base\Model;
+use common\models\User;
+
+/**
+ * Signup form
+ */
+class SignupForm extends Model
+{
+    public $username;
+    public $email;
+    public $password;
+    public $password_repeat;
+    public $reCaptcha;
+
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            ['username', 'trim'],
+            ['username', 'required'],
+            ['username', 'match', 'pattern' => '/^[a-z0-9_-]*[a-z0-9]+[a-z0-9_-]*$/i', 'message' => 'Only Latin letters, digits, underscore(_) and hyphen(-) are allowed.'],
+            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
+            ['username', 'string', 'min' => 2, 'max' => 20],
+
+            ['email', 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'string', 'max' => 255],
+            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+
+            ['password', 'required'],
+            ['password', 'string', 'min' => 6],
+            
+            ['password_repeat', 'required'],
+            ['password_repeat', 'compare', 'compareAttribute' => 'password'],
+            
+            [['reCaptcha'], \himiklab\yii2\recaptcha\ReCaptchaValidator::className(), 'secret' => Yii::$app->params['googleRecaptchaSecret']],
+            ['reCaptcha', 'required'],
+        ];
+    }
+
+    /**
+     * Signs user up.
+     *
+     * @return User|null the saved model or null if saving fails
+     */
+    public function signup()
+    {
+        if (!$this->validate()) {
+            return null;
+        }
+        
+        $user = new User();
+        $user->username = $this->username;
+        $user->email = $this->email;
+        $user->setPassword($this->password);
+        $user->generateAuthKey();
+        
+        return $user->save() ? $user : null;
+    }
+}
